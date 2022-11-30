ec9af764bd89e6a18c2a85c44d54094eaf52c2eb
António <oantoniomauricio@gmail.com>
2022-11-30 13:09:00 +0000

    v1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
 import './App.css';
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import example from './example.md';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Markdown />
+        <MarkdownComp />
       </header>
     </div>
   );
 }
 
-class Markdown extends React.Component {
+class MarkdownComp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      input: null,
     }
+
+  }
+
+  componentWillMount() {
+    fetch(example).then((response) => response.text()).then((text) => {
+      this.setState({ input: text })
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
+        <div className='flex flex-col items-center gap-y-10'>
 
-        <div className='bg-purple-600 flex flex-col'>
-          <div className='flex flex-row justify-between p-2'>
-            <h1 className="">Editor</h1>
-            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded inline-flex items-center">
-              <svg className="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
-            </button>
+          {/* Editor */}
+          <div className='bg-white flex flex-col w-2/4'>
+            <div className='flex flex-row justify-between p-2 bg-purple-600'>
+              <h1 className="">Editor</h1>
+              <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-2 rounded inline-flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3.8 3.8l16.4 16.4M20.2 3.8L3.8 20.2M15 3h6v6M9 3H3v6M15 21h6v-6M9 21H3v-6" /></svg>
+              </button>
+            </div>
+            <div className='flex flex-col'>
+              <textarea id="editor" value={this.state.input} onChange={this.handleChange.bind(this)} className='text-black flex' rows="5"></textarea>
+            </div>
           </div>
-          <div className='flex flex-row'>
-            <textarea id="editor" className='text-black'>
-              Teste
-            </textarea>
+
+
+          {/* Previwer */}
+          <div className='bg-white flex flex-col w-3/4'>
+            <div className='flex flex-row justify-between p-2 bg-purple-600'>
+              <h1>Previewer</h1>
+              <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-2 rounded inline-flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3.8 3.8l16.4 16.4M20.2 3.8L3.8 20.2M15 3h6v6M9 3H3v6M15 21h6v-6M9 21H3v-6" /></svg>
+              </button>
+            </div>
+            <div className='w-3/4'>
+              <p id='preview' className='text-black text-left prose max-w-none'><ReactMarkdown children={this.state.input} remarkPlugins={[remarkGfm]} /></p>
+            </div>
           </div>
+
         </div>
       </div >
     )
