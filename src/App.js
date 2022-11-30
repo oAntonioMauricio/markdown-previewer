8e6612529aab17b2dcffc169d87f8cbb5a53abab
António <oantoniomauricio@gmail.com>
2022-11-30 16:34:42 +0000

    buttons fixed

--- src/App.js
+++ src/App.js
@@ -40,7 +40,13 @@ class MarkdownEditor extends React.Component {
 
   handleDisplay() {
     this.setState({
-      state: !this.state.display
+      displayPrev: !this.state.displayPrev
+    })
+  }
+
+  handleDisplayEditor() {
+    this.setState({
+      displayEditor: !this.state.displayEditor
     })
   }
 
@@ -54,7 +60,7 @@ class MarkdownEditor extends React.Component {
             <div className='bg-white flex flex-col w-2/4'>
               <div className='flex flex-row justify-between p-2 bg-purple-600'>
                 <h1 className="">Editor</h1>
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-2 rounded inline-flex items-center">
+                <button onClick={this.handleDisplay.bind(this)} className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-2 rounded inline-flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3.8 3.8l16.4 16.4M20.2 3.8L3.8 20.2M15 3h6v6M9 3H3v6M15 21h6v-6M9 21H3v-6" /></svg>
                 </button>
               </div>
@@ -63,12 +69,22 @@ class MarkdownEditor extends React.Component {
               </div>
             </div>
 
-            <MarkdownPrev dataText={this.state.input} />
+            <MarkdownPrev dataText={this.state.input} dataDisplay={this.state.displayPrev} handlePrev={this.handleDisplayEditor.bind(this)} />
 
           </div>
         </div >
       )
+    } else {
+      return (
+        <div>
+          <div className='flex flex-col items-center gap-y-10 m-5'>
+            <MarkdownPrev dataText={this.state.input} dataDisplay={this.state.displayPrev} handlePrev={this.handleDisplayEditor.bind(this)} />
+          </div>
+        </div >
+      )
     }
+
+
   }
 }
 
@@ -82,22 +98,24 @@ class MarkdownPrev extends React.Component {
   }
 
   render() {
-    return (
-      <>
-        {/* Previwer */}
-        <div className='bg-white flex flex-col w-3/4'>
-          <div className='flex flex-row justify-between p-2 bg-purple-600'>
-            <h1>Previewer</h1>
-            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-2 rounded inline-flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3.8 3.8l16.4 16.4M20.2 3.8L3.8 20.2M15 3h6v6M9 3H3v6M15 21h6v-6M9 21H3v-6" /></svg>
-            </button>
+    if (this.props.dataDisplay) {
+      return (
+        <>
+          {/* Previwer */}
+          <div className='bg-white flex flex-col w-3/4'>
+            <div className='flex flex-row justify-between p-2 bg-purple-600'>
+              <h1>Previewer</h1>
+              <button onClick={this.props.handlePrev} className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-2 rounded inline-flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3.8 3.8l16.4 16.4M20.2 3.8L3.8 20.2M15 3h6v6M9 3H3v6M15 21h6v-6M9 21H3v-6" /></svg>
+              </button>
+            </div>
+            <div className='p-5'>
+              <p id='preview' className='text-black text-left break-words prose max-w-none'><ReactMarkdown children={this.props.dataText} remarkPlugins={[remarkGfm, remarkBreaks]} /></p>
+            </div>
           </div>
-          <div className='p-5'>
-            <p id='preview' className='text-black text-left break-words prose max-w-none'><ReactMarkdown children={this.props.dataText} remarkPlugins={[remarkGfm, remarkBreaks]} /></p>
-          </div>
-        </div>
-      </>
-    )
+        </>
+      )
+    }
   }
 }
 
